17de60822d0e287573e6bc7dfb6c96098c37486f
KlestD <fastvip57@gmail.com>
2026-03-12 16:50:51 +0100

    Speed up carousel reset to 45 seconds for faster reappearance

--- src/components/TrustMarquee.tsx
+++ src/components/TrustMarquee.tsx
@@ -23,7 +23,7 @@ const TrustMarquee = () => {
           className="flex gap-[65px] whitespace-nowrap"
           animate={{ x: -2248 }}
           transition={{
-            duration: 60,
+            duration: 45,
             repeat: Infinity,
             ease: "linear",
             repeatType: "loop"
